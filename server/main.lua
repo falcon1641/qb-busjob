1f4482daf12e560c0ab3d7a9517b06f4fb60027e
falcon1641 <94806141+falcon1641@users.noreply.github.com>
2022-01-17 15:26:03 -0500

    Change pay

--- server/main.lua
+++ server/main.lua
@@ -3,8 +3,8 @@ local QBCore = exports['qb-core']:GetCoreObject()
 RegisterNetEvent('qb-busjob:server:NpcPay', function(Payment)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local randomAmount = math.random(1, 5)
-    local r1, r2 = math.random(1, 5), math.random(1, 5)
-    if randomAmount == r1 or randomAmount == r2 then Payment = Payment + math.random(10, 20) end
+    local randomAmount = math.random(5, 5)
+    local r1, r2 = math.random(5, 5), math.random(5, 5)
+    if randomAmount == r1 or randomAmount == r2 then Payment = Payment + math.random(125, 300) end
     Player.Functions.AddMoney('cash', Payment)
 end)
